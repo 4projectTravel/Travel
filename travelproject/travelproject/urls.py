192d79a62fee50a7d93902666c3a87134d1aa2c0
g20944tr <g20944tr@gm.tsuda.ac.jp>
2023-10-22 15:33:53 +0900

    ’検索変更’

--- travelproject/travelproject/urls.py
+++ travelproject/travelproject/urls.py
@@ -11,7 +11,7 @@ from rest_framework import routers, serializers, viewsets
 class MapSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Map
-        fields = ('name', 'address', 'lat', 'lng', 'genre', 'ranking')
+        fields = ('name', 'address', 'lat', 'lng', 'genre', 'ranking','number')
 
 class MapViewSet(viewsets.ModelViewSet):
     #queryset = Map.objects.all()
