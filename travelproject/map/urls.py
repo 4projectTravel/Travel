01bfd1875f9078a8381e84a3c3de51ae9397f793
g20944tr <g20944tr@gm.tsuda.ac.jp>
2023-12-09 12:07:31 +0900

    レイアウト変更1209

--- travelproject/map/urls.py
+++ travelproject/map/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-   path('map_kamakura', views.map_kamakura, name='map_kamakura'),
+   path('kamakura/', views.map_kamakura, name='map_kamakura'),
 ]
